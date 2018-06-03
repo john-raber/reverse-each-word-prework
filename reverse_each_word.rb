d4903bae695d7c8d0f9a46e55a7aaed3e7c94877
John Raber <jrab224@gmail.com>
2018-06-03 17:00:41 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,5 +3,5 @@ def reverse_each_word(sentence)
   sentence.split(" ").each do |word|
     reversed_words << word.reverse
   end
-  revered_words.join(" ")
+  new_sentence = revered_words.join(" ")
 end
